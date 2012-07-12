557e465e6bda5895d09855c9bd1e24ecddf99207
GaoJiongjiong <gjj2828@gmail.com>
2012-07-12 09:47:08 +0000

    研究text

--- source/Render/RenderSystem.cpp
+++ source/Render/RenderSystem.cpp
@@ -21,8 +21,17 @@ void CRenderSystem::Update()
     pts[0].y = m_ClientRect.top;
     pts[1].x = m_ClientRect.right;
     pts[1].y = m_ClientRect.bottom;
-    HDC hdc = BeginPaint(m_hWnd, &ps);
+    HDC hdc = GetDC(m_hWnd);
+    //HPEN hpen = CreatePen(PS_SOLID, 5, COL_BLACK);
+    //SelectObject(hdc, hpen);
+    HFONT hfont = CreateFont(30, 30, 0, 0, FW_NORMAL, false, false, false, GB2312_CHARSET, OUT_CHARACTER_PRECIS, CLIP_CHARACTER_PRECIS, DEFAULT_QUALITY, FF_MODERN, NULL);
+    SelectObject(hdc, hfont);
     SetDCPenColor(hdc, COL_BLACK);
-    Polyline(hdc, pts, 2);
-    EndPaint(m_hWnd, &ps);
+    //RECT rect = {10, 10, 20, 20};
+    //DrawText(hdc, "0", 1, &rect, 0);
+    TextOut(hdc, 10, 10, "0", 1);
+    //Polyline(hdc, pts, 2);
+    ReleaseDC(m_hWnd, hdc);
+    DeleteObject(hfont);
+    //DeleteObject(hpen);
 }
